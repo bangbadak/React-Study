71697fe4ab6d6dd2ed8fdc869c7600665e9950cb
bangbadak <yosephlee050523@gmail.com>
2022-08-13 11:35:40 +0900

    movie app almost finished

---
+++ movie_app/src/App.js
@@ -0,0 +1,17 @@
+import Movie from "./components/Movie";
+import Home from "./routes/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/movie/:id" element={<Detail />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
